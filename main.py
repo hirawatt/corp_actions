23bc879daa2413bb956851c1ab50525e029b7921
VH <hirawatt@pm.me>
2021-02-22 17:41:30 +0530

    Updated with new data and added streamlit app visualisation.

--- main.py
+++ main.py
@@ -20,20 +20,33 @@ def corporates():
     mnth = today.strftime('%b').upper()
     dt = today.strftime('%d')
 
+    name_change = "https://www1.nseindia.com/content/equities/namechange.csv"
+    symbol_change = "https://www1.nseindia.com/content/equities/symbolchange.csv"
+
+    # corp_base_path data
     corp_base_path = "https://www1.nseindia.com/corporates/datafiles/"
 
     corp_announcements = "AN_LATEST_ANNOUNCED.csv"
     corp_actions = "CA_TODAY.csv"
-    name_change = "https://www1.nseindia.com/content/equities/namechange.csv"
-    symbol_change = "https://www1.nseindia.com/content/equities/symbolchange.csv"
     preference_shares = "pref_shares.csv"
     daily_buyback = "AN_NAV_LATEST_ANNOUNCED.csv"
     shareholding_patterns = "LatestShareholdings.csv"
+    # new set of data
+    acquisition = "AN_LATEST_ANNOUNCED_ACQUISITION.csv"
+    #amalgamation_merger = "AN_LATEST_ANNOUNCED_AMALGAMATION_MERGER.csv"
+    demerger = "AN_LATEST_ANNOUNCED_DEMERGER.csv"
+    #merger_acquisition = "AN_LATEST_ANNOUNCED_MERGER_ACQUISITION"
+    open_offer = "AN_LATEST_ANNOUNCED_OPEN_OFFER.csv"
+    scheme_of_arrangement = "AN_LATEST_ANNOUNCED_SCHEME_OF_ARRANGEMENT.csv"
 
     corp_data = [corp_announcements, corp_actions, daily_buyback, shareholding_patterns, preference_shares]
     other_data = [name_change, symbol_change]
+    # new data list
+    new_data = [acquisition, demerger, open_offer, scheme_of_arrangement]
 
-    for i in corp_data:
+    b = pd.DataFrame()
+    a = pd.DataFrame()
+    for i in new_data:
         dest = pwd +'/data/'+ i
         dest_url = corp_base_path + i
         print(dest, dest_url, sep='\n')
@@ -52,6 +65,9 @@ def corporates():
             "content": tabular_data
             }
         requests.post(discord_webhook_url_nse, data=msg)
+        b = pd.read_csv(dest)
+        a = a.append(b, ignore_index=True, sort=False)
+    return a
 
 def nse_equities_content(i):
     pwd = os.getcwd()
@@ -76,6 +92,6 @@ def nse_equities_content(i):
 
 if __name__ == '__main__':
     corporates()
-    nse_equities_content('bulk')
-    nse_equities_content('block')
-    nse_equities_content('ShortSelling')
+    #nse_equities_content('bulk')
+    #nse_equities_content('block')
+    #nse_equities_content('ShortSelling')
